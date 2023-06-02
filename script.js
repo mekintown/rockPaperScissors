126723317ec8c4c1dd5ade842076cb1cc4677a10
Maekin <74455990+mekintown@users.noreply.github.com>
2023-06-02 16:53:16 +0700

    Update UX/UI design

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-const rockBtn = document.querySelector(".rock")
-const paperBtn = document.querySelector(".paper")
-const scissorsBtn = document.querySelector(".scissors")
+const rockBtn = document.querySelector(".rockBtn");
+const paperBtn = document.querySelector(".paperBtn");
+const scissorsBtn = document.querySelector(".scissorsBtn");
+const scoreVal = document.querySelector(".scoreVal");
+const reportMessage = document.querySelector(".reportMessage");
+const robotHand = document.querySelector(".robotHand")
 
 const Scores = {player: 0, computer: 0}
 let round = 0;
 
 rockBtn.addEventListener('click', () => {
-    playSingleRound('rock', Scores);
+    reportMessage.textContent = playSingleRound('rock', Scores);
     updateGame();
 });
 paperBtn.addEventListener('click', () => {
-    playSingleRound("paper", Scores);
+    reportMessage.textContent = playSingleRound("paper", Scores);
     updateGame();
 });
 scissorsBtn.addEventListener('click', () => {
-    playSingleRound("scissors", Scores);
+    reportMessage.textContent = playSingleRound("scissors", Scores);
     updateGame();
 });
 
@@ -22,10 +25,13 @@ function getComputerChoice() {
     let choice = Math.floor(Math.random() * 100) % 3
     switch(choice) {
         case 0:
+            robotHand.className = 'fa-solid fa-hand-back-fist fa-2xl robotHand'
             return "rock";
         case 1:
+            robotHand.className = 'fa-solid fa-hand  fa-2xl robotHand'
             return "paper";
         default:
+            robotHand.className = 'fa-solid fa-hand-scissors fa-2xl robotHand'
             return "scissors"
     }
 }
@@ -49,10 +55,10 @@ function playSingleRound (playerSelection, scores, computerSelection=getComputer
     else if (playerSelection == "paper") {
         if (computerSelection == "scissors") {
             scores.computer += 1;
-            scores.player += 1;
             return "You Lose! Scissors beats Paper";
         }
         else if (computerSelection == "rock") {
+            scores.player += 1;
             return "You Win! Paper beats Rock";
         }
         else {
@@ -63,10 +69,10 @@ function playSingleRound (playerSelection, scores, computerSelection=getComputer
     else {
         if (computerSelection == "Rock") {
             scores.computer += 1;
-            scores.player += 1;
             return "You Lose! Rock beats Scissors";
         }
         else if (computerSelection == "paper") {
+            scores.player += 1;
             return "You Win! Scissors beats Paper";
         }
         else {
@@ -76,13 +82,13 @@ function playSingleRound (playerSelection, scores, computerSelection=getComputer
  }
 
 function updateGame() {
-    alert(`${Scores.player} | ${Scores.computer}`);
+    scoreVal.textContent = `${Scores.player} - ${Scores.computer}`
     if (Scores.player >= 5) {
-        alert("Player Win");
+        reportMessage.textContent = "You Win. Play Agian?";
         restartGame()
     }
     else if (Scores.computer >= 5){
-        alert("Player Lose");
+        reportMessage.textContent = "You Lose. Play Agian?";
         restartGame()
     }
  }
